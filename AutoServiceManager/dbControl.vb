efedd28b20aad0c8aaa7ce0af4d28773be4d8d9c
Nathan Litzinger <nlitz88@gmail.com>
2020-07-17 00:24:11 -0400

    Testing reading connection string from INI file

--- AutoServiceManager/dbControl.vb
+++ AutoServiceManager/dbControl.vb
@@ -1,18 +1,10 @@
-﻿' Import OleDb namespace
-Imports System.Data.OleDb
+﻿Imports System.Data.OleDb
+Imports System.IO
 
 Public Class DbControl
 
     ' New connection object. Connection string will be set in BuildConnString
     Private DbConn As New OleDbConnection()
-
-    ' Connection string parameters
-    Private DbProvider As String            ' The data provider (that includes the data adapter) that we want the connection to use to interface with the access database (in this case, Jet 4.0 for mDb files)
-    Private DbSource As String              ' The location of the actual access database file (will contain the complete composited location that we build with the following variables)
-
-    Private DbDirectory As String           ' The working directory where the database is currently setup/stored
-    Private DbFileName As String            ' The address of only the database
-    Private DbFullPath As String            ' Full path built from DatabaseDirectory and DatabaseFilename. Will be used in combination with another parameter to set DbSource Variable
 
     ' Database Command, DataAdapter, and DataTable for instance. Regenerated every query, so reference only.
     Private DbCmd As OleDbCommand
@@ -28,12 +20,12 @@ Public Class DbControl
 
     ' Default Constructor
     Public Sub New()
-        BuildConnString()                               ' might be replaced
+        GetConnStringFromINI()
     End Sub
 
     ' Constructor override for custom connection string
     Public Sub New(ByVal connectionString As String)
-        DbConn.ConnectionString = connectionString      ' might be replaced
+        DbConn.ConnectionString = connectionString
     End Sub
 
 
@@ -68,7 +60,7 @@ Public Class DbControl
             ' Setll all null values to defaults in newly filled DataTable
             SetNullsToDefault()
 
-            MessageBox.Show("Successfully queried " & DbFileName)
+            MessageBox.Show("Successfully queried database")
 
         Catch ex As Exception
 
@@ -106,15 +98,24 @@ Public Class DbControl
 
     ' Eventually, this will be the function that uses .config file to import these connection parameters
     ' Move this functionality to globals.vb? Or keep here...?
-    Private Sub BuildConnString()
+    Private Sub GetConnStringFromINI()
 
-        DbDirectory = "C:\Users\nlitz\Development\TMI Consulting\Auto Service Manager\AutoServiceManager\AutoServiceManager\Database"
-        DbFileName = "TMI-ServiceMgr.mDb"
-        DbFullPath = DbDirectory & "\" & DbFileName
-        DbSource = "Data Source = " & DbFullPath                ' Finally, define the DbSource by combinding everywith the appropriate parameter "Data Source"
-        DbProvider = "PROVIDER=Microsoft.Jet.OLEDB.4.0;"        ' Utilizing the Jet OLEDB data provider, as we are using an older access database .mDb (2003)
+        Dim line As String = String.Empty
+        Dim connString = String.Empty
+        Dim key As String = "CONNECTION-STRING="
+        Dim INIFile As New StreamReader(Application.StartupPath & "\connection.ini")
 
-        DbConn.ConnectionString = DbProvider & DbSource
+        Do Until INIFile.EndOfStream
+            line = INIFile.ReadLine()
+            If line.IndexOf(key) <> -1 Then
+                Exit Do
+            End If
+        Loop
+
+        If Not String.IsNullOrEmpty(line) Then
+            connString = line.Substring((line.IndexOf(key) + key.Length - 1), line.Length - 1)
+            Console.WriteLine(connString)
+        End If
 
     End Sub
 
