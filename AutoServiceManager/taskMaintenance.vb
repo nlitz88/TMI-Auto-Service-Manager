3db0be1caa23bd8b4f648eb88ffbd8beace52b69
Nathan Litzinger <nlitz88@gmail.com>
2020-07-29 13:59:26 -0400

    Successfully implement taskMaintenance. Considering adding different method of display task types in the drop down, but haven't implemented any solutions yet

--- AutoServiceManager/taskMaintenance.vb
+++ AutoServiceManager/taskMaintenance.vb
@@ -159,9 +159,19 @@ Public Class taskMaintenance
 
 
         ' Task Type Symbol (REQUIRED)(MUST BE UNIQUE)
+        Dim initialTTS As String = TTDbController.DbDataTable.Rows(TTRow)("TaskType").ToString().ToLower()
+        Console.WriteLine(initialTTS)
         If isEmpty("Task Type Symbol", True, TaskType_Textbox.Text, errorMessage) Then
             TaskType_Textbox.ForeColor = Color.Red
-        ElseIf isDuplicate("Payment Type Symbol", TaskType_Textbox.Text.ToLower(), errorMessage, TTList) Then
+            ' Before checking if it is a duplicate value, first ensure that it is not itself (the initial value)
+        ElseIf TaskType_Textbox.Text.ToLower() <> initialTTS Then
+            If isDuplicate("Task Type Symbol", TaskType_Textbox.Text.ToLower(), errorMessage, TTList) Then
+                TaskType_Textbox.ForeColor = Color.Red
+            End If
+        End If
+
+        ' Task Type Description (REQUIRED)
+        If isEmpty("Task Type Description", True, TaskType_Textbox.Text, errorMessage) Then
             TaskType_Textbox.ForeColor = Color.Red
         End If
 
@@ -181,12 +191,385 @@ Public Class taskMaintenance
 
     Private Sub taskMaintenance_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
+        ' TEST DATABASE CONNECTION FIRST
+        If Not checkDbConn() Then
+            MessageBox.Show("Failed to connect to database; Please restart and try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        ' LOAD DATATABLES FROM DATABASE INITIALLY
+        If Not loadDataTablesFromDatabase() Then
+            MessageBox.Show("Failed to connect to database; Please restart and try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+
+        ' SETUP CONTROLS HERE
+        TTComboBox.Items.Add("Select One")
+        For Each row In TTDbController.DbDataTable.Rows
+            TTComboBox.Items.Add(row("TaskType"))
+            'TTComboBox.Items.Add(row("TaskType") & " " & row("TaskDescription")), then split combobox.text on lookup by " "
+            ' Or, use custom sql query, and use the "Task Type + Task Description" as a separate column in the datatable, this way
+            ' we can lookup the values we want from the column values that we generated custom
+        Next
+        TTComboBox.SelectedIndex = 0
+
+    End Sub
+
+
+
+
+    ' **************** CONTROL SUBS ****************
+
+
+    ' Main eventhandler that handles most of the initialization for all subsequent elements/controls.
+    Private Sub TTCombobox_SelectedIndexChanged(sender As Object, e As EventArgs) Handles TTComboBox.SelectedIndexChanged, TTComboBox.TextChanged
+
+        ' If the input in the combobox matches an entry in the table that it represents
+        If TTList.BinarySearch(TTComboBox.Text.ToLower()) >= 0 Then
+
+            ' Initialize corresponding controls from DataTable values
+            valuesInitialized = False
+            InitializeTTDataViewingControls()
+            valuesInitialized = True
+
+            ' Show labels and corresponding values
+            showHide(getAllControlsWithTag("dataViewingControl", Me), 1)
+            showHide(getAllControlsWithTag("dataLabel", Me), 1)
+            showHide(getAllControlsWithTag("dataEditingControl", Me), 0)
+            ' Enable editing button
+            editButton.Enabled = True
+            deleteButton.Enabled = True
+
+        Else
+
+            ' Have all labels and corresponding values hidden
+            showHide(getAllControlsWithTag("dataViewingControl", Me), 0)
+            showHide(getAllControlsWithTag("dataLabel", Me), 0)
+            showHide(getAllControlsWithTag("dataEditingControl", Me), 0)
+            ' Disable editing button
+            editButton.Enabled = False
+            deleteButton.Enabled = False
+
+        End If
+
+    End Sub
+
+
+    Private Sub addButton_Click(sender As Object, e As EventArgs) Handles addButton.Click
+
+        mode = "adding"
+
+        ' Initialize values for dataEditingControls
+        clearControls(getAllControlsWithTag("dataEditingControl", Me))
+        ' Establish initial values. Doing this here, as unless changes are about to be made, we don't need to set initial values
+        InitialTTValues.SetInitialValues(getAllControlsWithTag("dataEditingControl", Me))
+
+        ' First, disable editButton, addButton, enable cancelButton, and disable nav
+        editButton.Enabled = False
+        addButton.Enabled = False
+        cancelButton.Enabled = True
+        nav.DisableAll()
+        TTComboBox.Enabled = False
+
+
+        lastSelected = TTComboBox.Text
+        TTComboBox.SelectedIndex = 0
+
+        ' Hide/Show the dataViewingControls and dataEditingControls, respectively
+        showHide(getAllControlsWithTag("dataViewingControl", Me), 0)
+        showHide(getAllControlsWithTag("dataEditingControl", Me), 1)
+        showHide(getAllControlsWithTag("dataLabel", Me), 1)
+
+    End Sub
+
+
+    Private Sub deleteButton_Click(sender As Object, e As EventArgs) Handles deleteButton.Click
+
+        Dim decision As DialogResult = MessageBox.Show("Are you sure?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+        Select Case decision
+            Case DialogResult.Yes
+
+                ' 1.) Delete value from database
+                If Not deleteAll() Then
+                    MessageBox.Show("Delete unsuccessful; Changes not saved", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                Else
+                    MessageBox.Show("Successfully deleted " & TTComboBox.Text & " from Task Types")
+                End If
+
+                ' 2.) RELOAD DATATABLES FROM DATABASE
+                If Not loadDataTablesFromDatabase() Then
+                    MessageBox.Show("Loading updated information failed; Old values will be reflected. Please restart and try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                End If
+
+                ' 3.) REINITIALIZE CONTROLS FROM THIS POINT (still from selection index, however)
+                TTComboBox.Items.Clear()
+                TTComboBox.Items.Add("Select One")
+                For Each row In TTDbController.DbDataTable.Rows
+                    TTComboBox.Items.Add(row("TaskType"))
+                Next
+                TTComboBox.SelectedIndex = 0
+
+                ' 4.) RESTORE USER CONTROLS TO NON-EDITING/SELECTING STATE
+                TTComboBox.Enabled = True
+                addButton.Enabled = True
+                cancelButton.Enabled = False
+                saveButton.Enabled = False
+                nav.EnableAll()
+                ' Show/Hide the dataViewingControls and dataEditingControls, respectively
+                ' This will be done by changing the selectedIndex to 0. May have to fire event here manually.
+
+            Case DialogResult.No
+
+        End Select
+
+    End Sub
+
+
+    Private Sub editButton_Click(sender As Object, e As EventArgs) Handles editButton.Click
+
+        mode = "editing"
+
+        ' Initialize values for dataEditingControls
+        InitializeTTDataEditingControls()
+        ' Establish initial values. Doing this here, as unless changes are about to be made, we don't need to set initial values
+        InitialTTValues.SetInitialValues(getAllControlsWithTag("dataEditingControl", Me))
+
+        ' Disable editButton, disable addButton, enable cancel button, disable navigation, and disable main selection combobox
+        editButton.Enabled = False
+        addButton.Enabled = False
+        cancelButton.Enabled = True
+        nav.DisableAll()
+        TTComboBox.Enabled = False
+
+        ' Hide/Show the dataViewingControls and dataEditingControls, respectively
+        showHide(getAllControlsWithTag("dataViewingControl", Me), 0)
+        showHide(getAllControlsWithTag("dataEditingControl", Me), 1)
+
+    End Sub
+
+    Private Sub cancelButton_Click(sender As Object, e As EventArgs) Handles cancelButton.Click
+
+        ' Check for changes before cancelling. Don't need function here that calls all, as only working with one datatable
+        If InitialTTValues.CtrlValuesChanged() Then
+
+            Dim decision As DialogResult = MessageBox.Show("Cancel without saving changes?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+            Select Case decision
+                Case DialogResult.Yes
+
+                    If mode = "editing" Then
+
+                        ' REINITIALIZE ALL CONTROL VALUES (as unwanted changes have been made)
+                        valuesInitialized = False
+                        InitializeAllDataEditingControls()
+                        valuesInitialized = True
+
+                        ' RESTORE USER CONTROLS TO NON-EDITING STATE
+                        editButton.Enabled = True
+                        addButton.Enabled = True
+                        cancelButton.Enabled = False
+                        saveButton.Enabled = False
+                        nav.EnableAll()
+                        TTComboBox.Enabled = True
+                        ' Show/Hide the dataViewingControls and dataEditingControls, respectively
+                        showHide(getAllControlsWithTag("dataViewingControl", Me), 1)
+                        showHide(getAllControlsWithTag("dataEditingControl", Me), 0)
+
+                    ElseIf mode = "adding" Then
+
+                        ' 1.) CLEAR DATA EDITING CONTROLS
+                        clearControls(getAllControlsWithTag("dataEditingControl", Me))
+                        'showHide(getAllControlsWithTag("dataEditingControl", Me), 0)
+
+                        ' 2.) SET TTComboBox BACKK TO LAST SELECTED ITEM/INDEX
+                        TTComboBox.SelectedIndex = TTComboBox.Items.IndexOf(lastSelected)
+                        ' If this is not select one, because it changed from the orig to select one, and now back to orig,
+                        ' the .textChanged event for the combo box will take care of reinitializing and showing the dataViewingControls
+
+                        ' 3.) IF LAST SELECTED WAS "SELECT ONE", Then simulate functionality from combobox text/selectedIndex changed
+                        If lastSelected = "Select One" Then
+                            TTCombobox_SelectedIndexChanged(TTComboBox, New EventArgs())
+                        End If
+
+                        ' 4.) RESTORE USER CONTROLS TO NON-ADDING STATE (only those that are controlled by "adding")
+                        TTComboBox.Enabled = True
+                        addButton.Enabled = True
+                        cancelButton.Enabled = False
+                        saveButton.Enabled = False
+                        nav.EnableAll()
+
+                    End If
+
+                Case DialogResult.No
+            End Select
+
+        Else
+
+            If mode = "editing" Then
+
+                ' RESTORE USER CONTROLS TO NON-EDITING STATE
+                editButton.Enabled = True
+                addButton.Enabled = True
+                cancelButton.Enabled = False
+                saveButton.Enabled = False
+                nav.EnableAll()
+                TTComboBox.Enabled = True
+                ' Show/Hide the dataViewingControls and dataEditingControls, respectively
+                showHide(getAllControlsWithTag("dataViewingControl", Me), 1)
+                showHide(getAllControlsWithTag("dataEditingControl", Me), 0)
+
+            ElseIf mode = "adding" Then
+
+                ' 1.) CLEAR DATA EDITING CONTROLS
+                clearControls(getAllControlsWithTag("dataEditingControl", Me))
+                'showHide(getAllControlsWithTag("dataEditingControl", Me), 0)
+
+                ' 2.) SET TTComboBox BACK TO LAST SELECTED ITEM/INDEX
+                TTComboBox.SelectedIndex = TTComboBox.Items.IndexOf(lastSelected)
+                ' If this is not select one, because it changed from the orig to select one, and now back to orig,
+                ' the .textChanged event for the combo box will take care of reinitializing and showing the dataViewingControls
+
+                ' 3.) IF LAST SELECTED WAS "SELECT ONE", Then simulate functionality from combobox text/selectedIndex changed
+                If lastSelected = "Select One" Then
+                    TTCombobox_SelectedIndexChanged(TTComboBox, New EventArgs())
+                End If
+
+                ' 4.) RESTORE USER CONTROLS TO NON-ADDING STATE (only those that are controlled by "adding")
+                TTComboBox.Enabled = True
+                addButton.Enabled = True
+                cancelButton.Enabled = False
+                saveButton.Enabled = False
+                nav.EnableAll()
+
+            End If
+
+
+
+        End If
+
+    End Sub
+
+
+    Private Sub saveButton_Click(sender As Object, e As EventArgs) Handles saveButton.Click
+
+        Dim decision As DialogResult = MessageBox.Show("Save Changes?", "Confirm Changes", MessageBoxButtons.YesNo)
+
+        Select Case decision
+            Case DialogResult.Yes
+
+                If mode = "editing" Then
+
+                    ' 1.) VALIDATE DATAEDITING CONTROLS
+                    If Not controlsValid() Then Exit Sub
+
+                    ' 2.) UPDATE DATATABLE(S), THEN UPDATE DATABASE
+                    If Not updateAll() Then
+                        MessageBox.Show("Update unsuccessful; Changes not saved", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Else
+                        MessageBox.Show("Successfully updated Task Types")
+                    End If
+
+                    ' 3.) RELOAD DATATABLES FROM DATABASE
+                    If Not loadDataTablesFromDatabase() Then
+                        MessageBox.Show("Loading updated information failed; Old values will be reflected. Please restart and try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+
+                    ' 4.) REINITIALIZE CONTROLS FROM THIS POINT (still from selection index, however)
+                    TTComboBox.Items.Clear()
+                    TTComboBox.Items.Add("Select One")
+                    For Each row In TTDbController.DbDataTable.Rows
+                        TTComboBox.Items.Add(row("TaskType"))
+                    Next
+                    TTComboBox.SelectedIndex = TTComboBox.Items.IndexOf(TaskType_Textbox.Text)
+                    'TTComboBox.SelectedIndex = amRow + 1
+
+                    ' dataViewingControl values reinitialized, as well as dataControls hide/show in combobox text/selectedindex change event
+
+                    ' 5.) MOVE UI OUT OF EDITING MODE
+                    addButton.Enabled = True
+                    cancelButton.Enabled = False
+                    saveButton.Enabled = False
+                    nav.EnableAll()
+                    TTComboBox.Enabled = True
+
+                ElseIf mode = "adding" Then
+
+                    ' 1.) VALIDATE DATAEDITING CONTROLS
+                    If Not controlsValid() Then Exit Sub
+
+                    ' 2.) INSERT NEW ROW INTO DATABASE
+                    If Not insertAll() Then
+                        MessageBox.Show("Insert unsuccessful; Changes not saved", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    Else
+                        MessageBox.Show("Successfully added " & TaskType_Textbox.Text & " to Task Types")
+                    End If
+
+                    ' 3.) RELOAD DATATABLES FROM DATABASE
+                    If Not loadDataTablesFromDatabase() Then
+                        MessageBox.Show("Loading updated information failed; Old values will be reflected. Please restart and try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+                    End If
+
+                    ' 4.) REINITIALIZE CONTROLS (based on index of newly inserted value)
+                    TTComboBox.Items.Clear()
+                    TTComboBox.Items.Add("Select One")
+                    For Each row In TTDbController.DbDataTable.Rows
+                        TTComboBox.Items.Add(row("TaskType"))
+                    Next
+
+                    ' Changing index of main combobox will also initialize respective dataViewing control values
+                    TTComboBox.SelectedIndex = TTComboBox.Items.IndexOf(TaskType_Textbox.Text)
+
+                    ' 5.) MOVE UI OUT OF Adding MODE
+                    addButton.Enabled = True
+                    cancelButton.Enabled = False
+                    saveButton.Enabled = False
+                    nav.EnableAll()
+                    TTComboBox.Enabled = True
+
+                End If
+
+            Case DialogResult.No
+                ' Continue making changes or cancel editing
+        End Select
 
 
     End Sub
+
+
+    Private Sub TaskType_Textbox_TextChanged(sender As Object, e As EventArgs) Handles TaskType_Textbox.TextChanged
+
+        If Not valuesInitialized Then Exit Sub
+
+        TaskType_Textbox.ForeColor = DefaultForeColor
+
+        If InitialTTValues.CtrlValuesChanged() Then
+            saveButton.Enabled = True
+        Else
+            saveButton.Enabled = False
+        End If
+
+    End Sub
+
+    Private Sub TaskDescription_Textbox_TextChanged(sender As Object, e As EventArgs) Handles TaskDescription_Textbox.TextChanged
+
+        If Not valuesInitialized Then Exit Sub
+
+        TaskType_Textbox.ForeColor = DefaultForeColor
+
+        If InitialTTValues.CtrlValuesChanged() Then
+            saveButton.Enabled = True
+        Else
+            saveButton.Enabled = False
+        End If
+
+    End Sub
+
 
     Private Sub taskMaintenance_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
         End
     End Sub
+
 
 End Class
