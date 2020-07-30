47704e97192cbacc568022c4f07b77d6245b62c7
Nathan Litzinger <nlitz88@gmail.com>
2020-07-30 15:04:32 -0400

    Added laborCodeMaintenance form. Will be adding to new branch after merging to master

---
+++ AutoServiceManager/laborCodeMaintenance.vb
@@ -0,0 +1,12 @@
+﻿Imports System.ComponentModel
+
+Public Class laborCodeMaintenance
+    Private Sub laborCodeMaintenance_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub laborCodeMaintenance_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
+        End
+    End Sub
+
+End Class
