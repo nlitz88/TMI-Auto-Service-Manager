6685792cc89dc3434690bd620e959c6da8c90274
Nathan Litzinger <nlitz88@gmail.com>
2020-08-07 23:01:59 -0400

    Added loading function for preliminary datatables, and added initialization function for TaskComboBox

--- AutoServiceManager/masterTaskMaintenance.vb
+++ AutoServiceManager/masterTaskMaintenance.vb
@@ -1,8 +1,11 @@
 ﻿Public Class masterTaskMaintenance
 
-    ' New Database control instances for Customers and ZipCodes datatable
+    ' New Database control instances for all preliminary datatables
     Private TaskDbController As New DbControl()
-    Private TaskTypeDbController As New DbControl()
+    Private TaskTypesDbController As New DbControl()
+    ' New Database control instances for all selection-dependent data
+    Private TaskLaborDbController As New DbControl()
+    Private TaskPartsDbController As New DbControl()
     ' New Database control instance for updating, inserting, and deleting
     Private CRUD As New DbControl()
 
@@ -14,8 +17,14 @@
 
     ' Row index variables used for DataTable lookups
     Private TaskRow As Integer
+    Private TaskLaborRow As Integer
+    Private TaskPartsRow As Integer
     Private TaskTypeRow As Integer
-    Private lastSelected As String
+
+    ' Last selected variables for reinitialization of controls from DataTables
+    Private lastSelectedTask As String
+    Private lastSelectedTaskLabor As String
+    Private lastSelectedTaskPart As String
 
     ' Keeps track of whether or not user in "editing" or "adding" mode
     Private mode As String
@@ -23,7 +32,47 @@
     ' Variable that allows certain keystrokes through restricted fields
     Private allowedKeystroke As Boolean = False
 
+
+
+
+    ' ***************** INITIALIZATION AND CONFIGURATION SUBS *****************
+
+
+    ' Function that will load all preliminary datatables
+    Private Function loadAllPreliminaryDataTables() As Boolean
+
+        ' Loads datatable from MasterTaskList table
+        TaskDbController.ExecQuery("SELECT mtl.TaskId, mtl.TaskDescription, mtl.Instructions, mtl.TaskType, mtl.TaskLabor, mtl.TaskParts, mtl.TotalTask " &
+                                       "FROM MasterTaskList mtl " &
+                                       "WHERE Trim(mtl.TaskDescription) <> '' " &
+                                       "ORDER BY mtl.TaskDescription ASC")
+        If TaskDbController.HasException() Then Return False
+
+        ' Loads datatable from TaskTypes
+        TaskTypesDbController.ExecQuery("SELECT mtl.TaskType, mtl.TaskDescription FROM TaskTypes mtl ORDER BY mtl.TaskType ASC")
+        If TaskTypesDbController.HasException() Then Return False
+
+        Return True
+
+    End Function
+
+
+    ' Sub that initializes Customer ComboBox
+    Private Sub InitializeCustomerComboBox()
+
+        TaskComboBox.BeginUpdate()
+        TaskComboBox.Items.Clear()
+        TaskComboBox.Items.Add("Select One")
+        For Each row In TaskDbController.DbDataTable.Rows
+            TaskComboBox.Items.Add(row("TaskDescription"))
+        Next
+        TaskComboBox.EndUpdate()
+
+    End Sub
+
+
     Private Sub masterTaskMaintenance_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
 End Class
