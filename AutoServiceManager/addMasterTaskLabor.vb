631a45969133240b3958c33c90cbfba4790ec991
Nathan Litzinger <nlitz88@gmail.com>
2020-08-14 01:53:22 -0400

    Added control subs for major buttons

--- AutoServiceManager/addMasterTaskLabor.vb
+++ AutoServiceManager/addMasterTaskLabor.vb
@@ -274,15 +274,91 @@ Public Class addMasterTaskLabor
 
     End Sub
 
+
+
     Private Sub addMasterTaskLabor_Closing(sender As Object, e As CancelEventArgs) Handles Me.Closing
 
+        If Not MeClosed Then
+
+            If validSelection Then
+
+                Dim decision As DialogResult = MessageBox.Show("Cancel without adding part?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+                If decision = DialogResult.No Then
+                    e.Cancel = True
+                    Exit Sub
+                Else
+                    MeClosed = True
+                    changeScreen(masterTaskMaintenance, Me)
+                End If
+
+            Else
+
+                MeClosed = True
+                changeScreen(masterTaskMaintenance, Me)
+
+            End If
+
+        End If
+
     End Sub
+
+
 
     Private Sub cancelButton_Click(sender As Object, e As EventArgs) Handles cancelButton.Click
 
+        If Not MeClosed Then
+
+            If validSelection Then
+
+                Dim decision As DialogResult = MessageBox.Show("Cancel without adding part?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+
+                If decision = DialogResult.No Then
+                    Exit Sub
+                Else
+                    MeClosed = True
+                    changeScreen(masterTaskMaintenance, Me)
+                End If
+
+            Else
+
+                MeClosed = True
+                changeScreen(masterTaskMaintenance, Me)
+
+            End If
+
+        End If
+
     End Sub
+
+
 
     Private Sub saveButton_Click(sender As Object, e As EventArgs) Handles saveButton.Click
 
+        ' No confirmation for additions at this time. May implement in the future.
+
+        ' 1.) VALIDATE DATAEDITING CONTROLS
+        If Not controlsValid() Then Exit Sub
+
+        ' 2.) WRITE CHANGES TO DATABASE TABLE
+        If Not InsertMasterTaskLabor() Then
+            MessageBox.Show("Insert unsuccessful; Changes not saved", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+        End If
+
+        ' 3.) If this is successful, then:
+        '       a.) Reinitialize Dependents on masterTaskMaintenance
+        '       b.) If that is successful, then change screen
+        If Not masterTaskMaintenance.reinitializeDependents() Then
+            MessageBox.Show("Reloading of Master Task List Unsuccessful; Old values will be reflected. Please restart and try again", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            saveButton.Enabled = False
+            Exit Sub
+        End If
+
+        MeClosed = True
+        changeScreen(masterTaskMaintenance, Me)
+
     End Sub
+
+
 End Class
