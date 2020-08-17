ce3643f47a1e9e14ca535a0210ece6244388cd20
Nathan Litzinger <nlitz88@gmail.com>
2020-08-17 16:12:47 -0400

    Added initial variables to invoices.db

--- AutoServiceManager/invoices.vb
+++ AutoServiceManager/invoices.vb
@@ -1,9 +1,52 @@
 ﻿Public Class invoices
+
+    ' New Database control instances for Customers, Vehicles, and InvHdr datatables
+    Private CustomerDbController As New DbControl()
+    Private VehicleDbController As New DbControl()
+    Private InvDbController As New DbControl()
+    ' New Database control instances for Tasks and Payments (for calculating values locally)
+    Private InvLaborDbController As New DbControl()
+    Private InvPartsDbController As New DbControl()
+    Private InvPaymentsDbController As New DbControl()
+    ' New Database control instance for updating, inserting, and deleting
+    Private CRUD As New DbControl()
+
+    ' Initialize instance(s) of initialValues class
+    Private InitialInvValues As New InitialValues()
+
+    'Variable to keep track of whether form fully loaded or not
+    Private valuesInitialized As Boolean = True
+
+    ' Row index variables used for further queries/lookups
+    Private CustomerRow As Integer              ' Maintains row in DataTable that corresponds to currently selected CLFA
+    Private CustomerId As Integer               ' CustomerId from same row that ""
+
+    Private VehicleRow As Integer               ' Maintains row in DataTable that corresponds to the currently selected YMML
+    Private VehicleId As Integer                ' VehicleId from same row that ""
+
+    Private InvRow As Integer                   ' Maintains row in DataTable that corresponds to the currently selected InvoiceNbr
+    Private InvId As Integer                    ' InvNbr from same row that ""
+
+    ' Keeps track of whether or not user in "editing" or "adding" mode
+    Private mode As String
+
+    ' Variable that allows certain keystrokes through restricted fields
+    Private allowedKeystroke As Boolean = False
+
+
+    Public Sub New()
+
+        ' This call is required by the designer.
+        InitializeComponent()
+
+        ' Add any initialization after the InitializeComponent() call.
+
+    End Sub
+
     Private Sub invoices_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
 
-    Private Sub vehicleHistoryButton_Click(sender As Object, e As EventArgs) Handles vehicleHistoryButton.Click
 
-    End Sub
+
 End Class
