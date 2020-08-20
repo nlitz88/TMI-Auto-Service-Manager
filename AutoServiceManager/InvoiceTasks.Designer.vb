cdbb030351b83fb61d658a8a99a66c39f3976f02
Nathan Litzinger <nlitz88@gmail.com>
2020-08-20 11:23:36 -0400

    Added invoice tasks form and basic controls

---
+++ AutoServiceManager/InvoiceTasks.Designer.vb
@@ -0,0 +1,137 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class InvoiceTasks
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.deleteButton = New System.Windows.Forms.Button()
+        Me.invoiceTasksLabel = New System.Windows.Forms.Label()
+        Me.editButton = New System.Windows.Forms.Button()
+        Me.cancelButton = New System.Windows.Forms.Button()
+        Me.saveButton = New System.Windows.Forms.Button()
+        Me.addButton = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'deleteButton
+        '
+        Me.deleteButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.deleteButton.Enabled = False
+        Me.deleteButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.deleteButton.ForeColor = System.Drawing.Color.White
+        Me.deleteButton.Location = New System.Drawing.Point(216, 120)
+        Me.deleteButton.Name = "deleteButton"
+        Me.deleteButton.Size = New System.Drawing.Size(110, 30)
+        Me.deleteButton.TabIndex = 131
+        Me.deleteButton.Text = "Delete"
+        Me.deleteButton.UseVisualStyleBackColor = False
+        '
+        'invoiceTasksLabel
+        '
+        Me.invoiceTasksLabel.AutoSize = True
+        Me.invoiceTasksLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.invoiceTasksLabel.ForeColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.invoiceTasksLabel.Location = New System.Drawing.Point(94, 73)
+        Me.invoiceTasksLabel.Name = "invoiceTasksLabel"
+        Me.invoiceTasksLabel.Size = New System.Drawing.Size(200, 32)
+        Me.invoiceTasksLabel.TabIndex = 135
+        Me.invoiceTasksLabel.Text = "Invoice Tasks"
+        '
+        'editButton
+        '
+        Me.editButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.editButton.Enabled = False
+        Me.editButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.editButton.ForeColor = System.Drawing.Color.White
+        Me.editButton.Location = New System.Drawing.Point(332, 120)
+        Me.editButton.Name = "editButton"
+        Me.editButton.Size = New System.Drawing.Size(110, 30)
+        Me.editButton.TabIndex = 132
+        Me.editButton.Text = "Edit"
+        Me.editButton.UseVisualStyleBackColor = False
+        '
+        'cancelButton
+        '
+        Me.cancelButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.cancelButton.Enabled = False
+        Me.cancelButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.cancelButton.ForeColor = System.Drawing.Color.White
+        Me.cancelButton.Location = New System.Drawing.Point(563, 120)
+        Me.cancelButton.Name = "cancelButton"
+        Me.cancelButton.Size = New System.Drawing.Size(110, 30)
+        Me.cancelButton.TabIndex = 134
+        Me.cancelButton.Text = "Cancel"
+        Me.cancelButton.UseVisualStyleBackColor = False
+        '
+        'saveButton
+        '
+        Me.saveButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.saveButton.Enabled = False
+        Me.saveButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.saveButton.ForeColor = System.Drawing.Color.White
+        Me.saveButton.Location = New System.Drawing.Point(448, 120)
+        Me.saveButton.Name = "saveButton"
+        Me.saveButton.Size = New System.Drawing.Size(110, 30)
+        Me.saveButton.TabIndex = 133
+        Me.saveButton.Text = "Save"
+        Me.saveButton.UseVisualStyleBackColor = False
+        '
+        'addButton
+        '
+        Me.addButton.BackColor = System.Drawing.Color.FromArgb(CType(CType(46, Byte), Integer), CType(CType(138, Byte), Integer), CType(CType(46, Byte), Integer))
+        Me.addButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.addButton.ForeColor = System.Drawing.Color.White
+        Me.addButton.Location = New System.Drawing.Point(100, 120)
+        Me.addButton.Name = "addButton"
+        Me.addButton.Size = New System.Drawing.Size(110, 30)
+        Me.addButton.TabIndex = 130
+        Me.addButton.Tag = ""
+        Me.addButton.Text = "Add"
+        Me.addButton.UseVisualStyleBackColor = False
+        '
+        'InvoiceTasks
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(246, Byte), Integer), CType(CType(239, Byte), Integer), CType(CType(220, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(982, 753)
+        Me.Controls.Add(Me.deleteButton)
+        Me.Controls.Add(Me.invoiceTasksLabel)
+        Me.Controls.Add(Me.editButton)
+        Me.Controls.Add(Me.cancelButton)
+        Me.Controls.Add(Me.saveButton)
+        Me.Controls.Add(Me.addButton)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.MaximizeBox = False
+        Me.Name = "InvoiceTasks"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.Manual
+        Me.Text = "Invoice Tasks"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents deleteButton As Button
+    Friend WithEvents invoiceTasksLabel As Label
+    Friend WithEvents editButton As Button
+    Friend WithEvents cancelButton As Button
+    Friend WithEvents saveButton As Button
+    Friend WithEvents addButton As Button
+End Class
---
+++ AutoServiceManager/InvoiceTasks.vb
@@ -0,0 +1,3 @@
+﻿Public Class InvoiceTasks
+
+End Class
