f5aeec862ed3c3ed22e4556c9f0fe3fbb46dd59c
Nathan Litzinger <nlitz88@gmail.com>
2020-08-20 13:04:50 -0400

    Added variables to InvoiceTasks

--- AutoServiceManager/InvoiceTasks.vb
+++ AutoServiceManager/InvoiceTasks.vb
@@ -1,12 +1,45 @@
 ﻿Public Class InvoiceTasks
 
-    Private InitialITValues As New InitialValues()
+    ' New Database control instances for all preliminary datatables
+    Private InvTasksDbController As New DbControl()
+    ' New Database control instances for all selection-dependent data
+    Private InvTaskLaborDbController As New DbControl()
+    Private InvTaskPartsDbController As New DbControl()
+    ' New Database control instance for updating, inserting, and deleting
+    Private CRUD As New DbControl()
+
+    ' Initialize instance(s) of initialValues class
+    Private InitialInvTasksValues As New InitialValues()
+
+    'Variable to keep track of whether form fully loaded or not
+    Private valuesInitialized As Boolean = True
+
+    ' Row index variables used for DataTable lookups
+    Private InvTaskRow As Integer
+    Private InvTaskId As Integer
+
+    ' Row indexes for use with InvTaskLabor and InvTaskParts DataGridViews
+    Private InvTaskLaborRow As Integer = -1
+    Private InvTaskPartsRow As Integer = -1
+
+    ' Variables that store calculated values for InvTaskLabor, InvTaskParts, and InvTotalTask Values/Textboxes
+    Private InvTaskLaborSum As Decimal = 0
+    Private InvTaskPartsSum As Decimal = 0
+    Private InvTotalTaskSum As Decimal = 0
+
+    ' Last selected variables for reinitialization of controls from DataTables
+    Private lastSelectedInvTask As String
+    Private lastSelectedInvTaskLabor As String
+    Private lastSelectedInvTaskPart As String
+
+    ' Keeps track of whether or not user in "editing" or "adding" mode for various control groups
+    Private mode As String
+
+    ' Variable that allows certain keystrokes through restricted fields
+    Private allowedKeystroke As Boolean = False
 
     ' Boolean to keep track of whether or not this form has been closed
     Private MeClosed As Boolean = False
-
-
-    ' Initialize InvTaskComboBox with InvTaskNbr and InvTaskDescription
 
     Public Sub New()
 
@@ -32,7 +65,7 @@
         If Not MeClosed Then
 
             ' Check if editing/adding, and if editing/adding, check if control values changed
-            If TaskDescription_Textbox.Visible And InitialITValues.CtrlValuesChanged() Then
+            If TaskDescription_Textbox.Visible And InitialInvTasksValues.CtrlValuesChanged() Then
 
                 Dim decision As DialogResult = MessageBox.Show("Return to invoice without saving changes?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
 
@@ -60,7 +93,7 @@
         If Not MeClosed Then
 
             ' Check if editing/adding, and if editing/adding, check if control values changed
-            If TaskDescription_Textbox.Visible And InitialITValues.CtrlValuesChanged() Then
+            If TaskDescription_Textbox.Visible And InitialInvTasksValues.CtrlValuesChanged() Then
 
                 Dim decision As DialogResult = MessageBox.Show("Return to invoice without saving changes?", "Confirm", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
 
