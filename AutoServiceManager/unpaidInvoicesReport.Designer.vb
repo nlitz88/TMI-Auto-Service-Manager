f94a61516126c78e1c0b623e3b8882024a903584
Nathan Litzinger <nlitz88@gmail.com>
2020-09-19 23:08:49 -0400

    Finished implementing unpaidInvoicesReport

--- AutoServiceManager/unpaidInvoicesReport.Designer.vb
+++ AutoServiceManager/unpaidInvoicesReport.Designer.vb
@@ -22,27 +22,28 @@ Partial Class unpaidInvoicesReport
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ReportDateTextbox = New System.Windows.Forms.MaskedTextBox()
+        Me.StartDateTextbox = New System.Windows.Forms.MaskedTextBox()
         Me.ReportDateLabel = New System.Windows.Forms.Label()
         Me.previewReportButton = New System.Windows.Forms.Button()
         Me.UnpaidInvoicesReportLabel = New System.Windows.Forms.Label()
+        Me.nav = New AutoServiceManager.navigation()
         Me.SuspendLayout()
         '
-        'ReportDateTextbox
+        'StartDateTextbox
         '
-        Me.ReportDateTextbox.AllowPromptAsInput = False
-        Me.ReportDateTextbox.CutCopyMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals
-        Me.ReportDateTextbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!)
-        Me.ReportDateTextbox.InsertKeyMode = System.Windows.Forms.InsertKeyMode.Insert
-        Me.ReportDateTextbox.Location = New System.Drawing.Point(269, 138)
-        Me.ReportDateTextbox.Mask = "00/00/0000"
-        Me.ReportDateTextbox.Name = "ReportDateTextbox"
-        Me.ReportDateTextbox.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
-        Me.ReportDateTextbox.Size = New System.Drawing.Size(132, 27)
-        Me.ReportDateTextbox.TabIndex = 281
-        Me.ReportDateTextbox.Tag = "dataEditingControl"
-        Me.ReportDateTextbox.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals
-        Me.ReportDateTextbox.ValidatingType = GetType(Date)
+        Me.StartDateTextbox.AllowPromptAsInput = False
+        Me.StartDateTextbox.CutCopyMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals
+        Me.StartDateTextbox.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.2!)
+        Me.StartDateTextbox.InsertKeyMode = System.Windows.Forms.InsertKeyMode.Insert
+        Me.StartDateTextbox.Location = New System.Drawing.Point(269, 138)
+        Me.StartDateTextbox.Mask = "00/00/0000"
+        Me.StartDateTextbox.Name = "StartDateTextbox"
+        Me.StartDateTextbox.PromptChar = Global.Microsoft.VisualBasic.ChrW(32)
+        Me.StartDateTextbox.Size = New System.Drawing.Size(132, 27)
+        Me.StartDateTextbox.TabIndex = 281
+        Me.StartDateTextbox.Tag = "dataEditingControl"
+        Me.StartDateTextbox.TextMaskFormat = System.Windows.Forms.MaskFormat.IncludePromptAndLiterals
+        Me.StartDateTextbox.ValidatingType = GetType(Date)
         '
         'ReportDateLabel
         '
@@ -78,13 +79,24 @@ Partial Class unpaidInvoicesReport
         Me.UnpaidInvoicesReportLabel.TabIndex = 282
         Me.UnpaidInvoicesReportLabel.Text = "Unpaid Invoices Report"
         '
+        'nav
+        '
+        Me.nav.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.nav.Location = New System.Drawing.Point(0, 0)
+        Me.nav.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.nav.Name = "nav"
+        Me.nav.Size = New System.Drawing.Size(982, 28)
+        Me.nav.TabIndex = 285
+        '
         'unpaidInvoicesReport
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(246, Byte), Integer), CType(CType(239, Byte), Integer), CType(CType(220, Byte), Integer))
         Me.ClientSize = New System.Drawing.Size(982, 753)
-        Me.Controls.Add(Me.ReportDateTextbox)
+        Me.Controls.Add(Me.nav)
+        Me.Controls.Add(Me.StartDateTextbox)
         Me.Controls.Add(Me.ReportDateLabel)
         Me.Controls.Add(Me.previewReportButton)
         Me.Controls.Add(Me.UnpaidInvoicesReportLabel)
@@ -98,8 +110,9 @@ Partial Class unpaidInvoicesReport
 
     End Sub
 
-    Friend WithEvents ReportDateTextbox As MaskedTextBox
+    Friend WithEvents StartDateTextbox As MaskedTextBox
     Friend WithEvents ReportDateLabel As Label
     Friend WithEvents previewReportButton As Button
     Friend WithEvents UnpaidInvoicesReportLabel As Label
+    Friend WithEvents nav As navigation
 End Class
--- AutoServiceManager/unpaidInvoicesReport.vb
+++ AutoServiceManager/unpaidInvoicesReport.vb
@@ -1,3 +1,93 @@
 ﻿Public Class unpaidInvoicesReport
 
+
+
+
+    ' New Database control instance used for getting number of receipts
+    Private CRUD As New DbControl()
+
+
+
+
+    ' **************** VALIDATION SUBS ****************
+
+
+    Private Function controlsValid() As Boolean
+
+        Dim errorMessage As String = String.Empty
+
+
+        ' Report Date (REQUIRED)
+        If Not validDateTime("Report Date", True, StartDateTextbox.Text, errorMessage) Then
+            StartDateTextbox.ForeColor = Color.Red
+        End If
+
+
+        ' Check if any invalid input has been found
+        If Not String.IsNullOrEmpty(errorMessage) Then
+
+            MessageBox.Show(errorMessage, "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+            Return False
+
+        End If
+
+        Return True
+
+    End Function
+
+
+
+
+
+    Private Sub unpaidInvoicesReport_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        ' On load, populate ReportDateTextbox with current date
+        StartDateTextbox.Text = formatDate(DateTime.Now())
+
+    End Sub
+
+    Private Sub previewReportButton_Click(sender As Object, e As EventArgs) Handles previewReportButton.Click
+
+
+        ' 1.) Validate date
+        If Not controlsValid() Then Exit Sub
+
+        Try
+
+            ' 2.) Check if any invoices after provided date
+
+            ' Use CRUD to see if there are any invoices after the provided date
+            CRUD.AddParams("@firstdate", StartDateTextbox.Text)
+            CRUD.ExecQuery("SELECT InvNbr, InvDate FROM InvHdr WHERE InvDate >= @firstdate")
+
+            If Not CRUD.HasException(True) Then
+                If CRUD.DbDataTable.Rows.Count <> 0 Then
+
+                    ' 3.) Open report preview if entries made on provided date
+                    Dim AcccessInstance As New Microsoft.Office.Interop.Access.Application()
+                    Dim filepath As String = readINI("AutoServiceManagerParams.ini", "PRIMARY-DATABASE-FILEPATH=")
+
+                    AcccessInstance.Visible = False
+                    AcccessInstance.OpenCurrentDatabase(filepath)
+
+                    AcccessInstance.DoCmd.OpenReport(ReportName:="UnpaidInvoices", Microsoft.Office.Interop.Access.AcView.acViewPreview, , WhereCondition:="InvDate>=#" + CStr(StartDateTextbox.Text) + "#")
+
+                Else
+                    MessageBox.Show("No invoices found after " & StartDateTextbox.Text & ".", "No Invoices Found", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+                End If
+            Else
+                MessageBox.Show("Unable to load invoices. Please restart and try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End If
+
+        Catch ex As Exception
+
+            MessageBox.Show("Viewing preview unsuccessful. Ensure Database has not been moved and retry.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Exit Sub
+
+        End Try
+
+
+    End Sub
+
+
 End Class
